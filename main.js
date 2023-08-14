bd24754cb0eebd5892bee4ca3118a1d38d1db0ae
Ikemba Chibueze <95286772+sagites@users.noreply.github.com>
2023-08-14 23:35:50 +0100

    Improved the functionality of the page

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 const button1 = document.getElementById('popupBtn');
 const button2 = document.getElementById('popupBtn2');
 const popUp = document.getElementById('popup');
+const input = document.getElementById('input_email');
+const value = input.value;
 
-// button1.addEventListener('click', displayMessage(), false);
 
-function displayMessage() {
+button1.addEventListener('click', displayMessage(), false);
+
+function displayMessage(event) {
+    event.preventDefault();
     popUp.classList.add("popup");
 }
 
-// button2.addEventListener('click', removeMessage(), false)
+ button2.addEventListener('click', removeMessage(), false)
 function removeMessage() {
     popUp.classList.remove('popup');
 }
