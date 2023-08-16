5a145e7608cc20e35c715111a13eb5c7bef30892
Ikemba Chibueze <95286772+sagites@users.noreply.github.com>
2023-08-16 21:18:01 +0100

    Little changes here and there

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
 const button1 = document.getElementById('popupBtn');
 const button2 = document.getElementById('popupBtn2');
 const popUp = document.getElementById('popup');
-const input = document.getElementById('input_email');
-const value = input.value;
+const input = document.getElementById('inputEmail');
+const mail = document.querySelector('#mail b');
+const show = document.querySelector('.container');
+// const image = document.querySelector('section .image')
 
+button1.addEventListener('click', () => {
+    popUp.classList.add('popup');
+    show.classList.add('show');
+    mail.innerText = input.value;
+    clearInput();
+}, false);
 
-button1.addEventListener('click', displayMessage(), false);
+ button2.addEventListener('click', () => {
+    popUp.classList.remove('popup')
+    show.classList.remove('show');
+ }, false);
 
-function displayMessage(event) {
-    // event.preventDefault();
-    popUp.classList.add("popup");
-}
-
- button2.addEventListener('click', removeMessage(), false)
-function removeMessage() {
-    popUp.classList.remove('popup');
+function clearInput() {
+    input.value = '';
 }
